99d0a70964e223ea935e66101150b0289317e904
Durga Nepal <durgain.np@gmail.com>
2021-11-14 12:57:47 +0900

    Load product from bckend

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadingBox() {
+  return (
+    <div>
+      <i className="fa fa-spinner" aria-hidden="true">
+        Loading...
+      </i>
+    </div>
+  );
+}
+
+export default LoadingBox;
